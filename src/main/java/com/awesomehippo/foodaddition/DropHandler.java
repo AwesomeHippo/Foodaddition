6b4bb3fa7d5f8cb05564debcd70fb1b48ebb7d0e
Thenemex <tnmx777@gmail.com>
2025-04-28 10:08:55 +0200

    Added javadoc in DropHandler class

--- src/main/java/com/awesomehippo/foodaddition/DropHandler.java
+++ src/main/java/com/awesomehippo/foodaddition/DropHandler.java
@@ -14,6 +14,9 @@ public class DropHandler {
     private final HashMap<String, Item> rawDropMap = new HashMap<>(), cookedDropMap = new HashMap<>();
     private static final Random random = new Random();
 
+    /**
+     * Contructor of the class - will only put the right keys and values in the Item's Maps
+     */
     public DropHandler(){
         rawDropMap.put("Sheep", FoodItems.rawMutton);
         rawDropMap.put("Horse", FoodItems.rawHorse);
@@ -25,12 +28,18 @@ public class DropHandler {
         cookedDropMap.put("Wolf", FoodItems.cookedWolf);
     }
 
+    /**
+     * Method triggered when a mob is killed and will be dropping items
+     */
     @SubscribeEvent
     public void onEntityDrops(LivingDropsEvent event) {
         genTotalDrops(event.lootingLevel);
         event.entityLiving.entityDropItem(getDrops(event.entityLiving), 0.0F);
     }
 
+    /**
+     * Returns the type of item dropped and it's count, depending on the entity
+     */
     private ItemStack getDrops(Entity entity) {
         String entityName = entity.getClass().getSimpleName().substring(6); // EntitySheep -> Sheep
         Item itemDropped = entity.isBurning()
@@ -39,6 +48,10 @@ public class DropHandler {
         return new ItemStack(itemDropped, totalDrops);
     }
 
+    /**
+     * Calculate the number of items dropped by the mob killed
+     * @param looting The looting level of the player's weapon
+     */
     private void genTotalDrops(int looting) {
         this.totalDrops = random.nextInt(2) + 1 + (looting > 0 ? random.nextInt(looting + 1) : 0);
 
