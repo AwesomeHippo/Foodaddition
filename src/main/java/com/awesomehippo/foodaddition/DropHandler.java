6e7f6a2cd4e0c22f6f4ec07dbffd87c584562cb8
Thenemex <tnmx777@gmail.com>
2025-04-28 09:57:07 +0200

    Refactoring code - Replaced the elif block by two HashMaps (raw and cooked meat)

--- src/main/java/com/awesomehippo/foodaddition/DropHandler.java
+++ src/main/java/com/awesomehippo/foodaddition/DropHandler.java
@@ -1,55 +1,46 @@
 package com.awesomehippo.foodaddition;
 
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
-import net.minecraft.entity.passive.EntitySheep;
-import net.minecraft.entity.passive.EntityHorse;
-import net.minecraft.entity.passive.EntitySquid;
-import net.minecraft.entity.passive.EntityWolf;
+import net.minecraft.entity.Entity;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.event.entity.living.LivingDropsEvent;
 
+import java.util.HashMap;
 import java.util.Random;
 
 public class DropHandler {
+    private int totalDrops;
+    private final HashMap<String, Item> rawDropMap = new HashMap<>(), cookedDropMap = new HashMap<>();
     private static final Random random = new Random();
 
+    public DropHandler(){
+        rawDropMap.put("Sheep", FoodItems.rawMutton);
+        rawDropMap.put("Horse", FoodItems.rawHorse);
+        rawDropMap.put("Squid", FoodItems.rawCalamari);
+        rawDropMap.put("Wolf", FoodItems.rawWolf);
+        cookedDropMap.put("Sheep", FoodItems.cookedMutton);
+        cookedDropMap.put("Horse", FoodItems.cookedHorse);
+        cookedDropMap.put("Squid", FoodItems.cookedCalamari);
+        cookedDropMap.put("Wolf", FoodItems.cookedWolf);
+    }
+
     @SubscribeEvent
-    public void onEntityDrop(LivingDropsEvent event) {
-        if (event.entityLiving instanceof EntitySheep) {
-            int baseAmount = random.nextInt(2) + 1;
-            int lootingBonus = event.lootingLevel > 0 ? random.nextInt(event.lootingLevel + 1) : 0;
-            int totalDrops = baseAmount + lootingBonus;
-            ItemStack drop = event.entityLiving.isBurning()
-                    ? new ItemStack(FoodItems.cookedMutton, totalDrops)
-                    : new ItemStack(FoodItems.rawMutton, totalDrops);
-            event.entityLiving.entityDropItem(drop, 0.0F);
+    public void onEntityDrops(LivingDropsEvent event) {
+        genTotalDrops(event.lootingLevel);
+        event.entityLiving.entityDropItem(getDrops(event.entityLiving), 0.0F);
+    }
 
-        } else if (event.entityLiving instanceof EntityHorse) {
-            int baseAmount = random.nextInt(2) + 1;
-            int lootingBonus = event.lootingLevel > 0 ? random.nextInt(event.lootingLevel + 1) : 0;
-            int totalDrops = baseAmount + lootingBonus;
-            ItemStack drop = event.entityLiving.isBurning()
-                    ? new ItemStack(FoodItems.cookedHorse, totalDrops)
-                    : new ItemStack(FoodItems.rawHorse, totalDrops);
-            event.entityLiving.entityDropItem(drop, 0.0F);
+    private ItemStack getDrops(Entity entity) {
+        String entityName = entity.getClass().getSimpleName().substring(6); // EntitySheep -> Sheep
+        Item itemDropped = entity.isBurning()
+                ? cookedDropMap.get(entityName)
+                : rawDropMap.get(entityName);
+        return new ItemStack(itemDropped, totalDrops);
+    }
 
-        } else if (event.entityLiving instanceof EntitySquid) {
-            int baseAmount = random.nextInt(2) + 1;
-            int lootingBonus = event.lootingLevel > 0 ? random.nextInt(event.lootingLevel + 1) : 0;
-            int totalDrops = baseAmount + lootingBonus;
-            ItemStack drop = event.entityLiving.isBurning()
-                    ? new ItemStack(FoodItems.cookedCalamari, totalDrops)
-                    : new ItemStack(FoodItems.rawCalamari, totalDrops);
-            event.entityLiving.entityDropItem(drop, 0.0F);
+    private void genTotalDrops(int looting) {
+        this.totalDrops = random.nextInt(2) + 1 + (looting > 0 ? random.nextInt(looting + 1) : 0);
 
-        } else if (event.entityLiving instanceof EntityWolf) {
-            int baseAmount = random.nextInt(2) + 1;
-            int lootingBonus = event.lootingLevel > 0 ? random.nextInt(event.lootingLevel + 1) : 0;
-            int totalDrops = baseAmount + lootingBonus;
-            ItemStack drop = event.entityLiving.isBurning()
-                    ? new ItemStack(FoodItems.cookedWolf, totalDrops)
-                    : new ItemStack(FoodItems.rawWolf, totalDrops);
-            event.entityLiving.entityDropItem(drop, 0.0F);
-        }
     }
 }
