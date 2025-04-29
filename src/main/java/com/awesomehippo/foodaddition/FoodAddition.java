208bcdeca5c85078c3820da87342752c6f0e3113
Thenemex <tnmx777@gmail.com>
2025-04-29 17:01:34 +0200

    Reorganized code (preinit, init, postinit) - Removed duplicated licence file

--- src/main/java/com/awesomehippo/foodaddition/FoodAddition.java
+++ src/main/java/com/awesomehippo/foodaddition/FoodAddition.java
@@ -5,16 +5,25 @@ import cpw.mods.fml.common.Loader;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.common.MinecraftForge;
 
 @Mod(modid = "sheepfood", name = "Food Addition", version = "1.5", acceptedMinecraftVersions = "[1.7.10]")
 public class FoodAddition {
 
     @EventHandler
-    public void initialize(FMLInitializationEvent event) {
+    public void preInit(FMLPreInitializationEvent event) {
         new FoodItems();
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event) {
         MinecraftForge.EVENT_BUS.register(new DropHandler());
-        // Loading integrations
+    }
+
+    @EventHandler
+    public void postInit(FMLPostInitializationEvent event) {
         if (Loader.isModLoaded("Thaumcraft")) new ThaumcraftCompat();
     }
 }
