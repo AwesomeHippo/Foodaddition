d9501fd377aa261712f7c4777c2691241c157115
pseudonym-2669 <rob1.tisserand@gmail.com>
2025-05-10 16:49:31 +0200

    Refactor and use a hashmap

--- src/main/java/foodaddition/api/handlers/PotionEffectHandler.java
+++ src/main/java/foodaddition/api/handlers/PotionEffectHandler.java
@@ -20,7 +20,8 @@ import java.util.*;
 
 public class PotionEffectHandler {
 
-    private static final List<FoodEffectEntry> configEntries = new ArrayList<>();
+    // hashmap
+    private static final Map<String, List<PotionEffect>> effectMap = new HashMap<>();
 
     public PotionEffectHandler(File configDir) {
         //TODO: we could also put both configs files in a custom dir like HLC
@@ -35,25 +36,10 @@ public class PotionEffectHandler {
         String registryName = Item.itemRegistry.getNameForObject(eatenStack.getItem());
         int meta = eatenStack.getItemDamage();
 
-        for (FoodEffectEntry entry : configEntries) {
-            if (entry.item.equals(registryName) && entry.meta == meta) {
-                //  without addPotionEffectToPlayer
-                /*for (EffectEntry effect : entry.effects) {
-                    PotionEffect potionEffect = new PotionEffect(
-                            effect.id,
-                            effect.duration * 20,
-                            effect.amplifier
-                    );
-                    event.entityPlayer.addPotionEffect(potionEffect);
-                }*/
-                // with addPotionEffectToPlayer
-                List<PotionEffect> effects = new ArrayList<>();
-                for (EffectEntry effect : entry.effects) {
-                    effects.add(new PotionEffect(effect.id, effect.duration * 20, effect.amplifier));
-                }
-                addPotionEffectToPlayer(event.entityPlayer, effects);
-                break;
-            }
+        String key = registryName.concat("@").concat(String.valueOf(meta));
+        List<PotionEffect> effects = effectMap.get(key);
+        if (effects != null) {
+            addPotionEffectToPlayer(event.entityPlayer, effects);
         }
     }
 
@@ -64,15 +50,16 @@ public class PotionEffectHandler {
 
                 // default config for apple -> regen II for 20 seconds lol - TODO: change?
                 // TODO: just initialize it as empty?
-                String defaultJson = "[\n" +
-                        "  {\n" +
-                        "    \"item\": \"minecraft:apple\",\n" +
-                        "    \"meta\": 0,\n" +
-                        "    \"effects\": [\n" +
-                        "      { \"id\": 10, \"duration\": 20, \"amplifier\": 1 }\n" +
-                        "    ]\n" +
-                        "  }\n" +
-                        "]";
+                String defaultJson = "[\n".concat(
+                        "  {\n").concat(
+                        "    \"item\": \"minecraft:apple\",\n").concat(
+                        "    \"meta\": 0,\n").concat(
+                        "    \"effects\": [\n").concat(
+                        "      { \"id\": 10, \"duration\": 20, \"amplifier\": 1 }\n").concat(
+                        "    ]\n").concat(
+                        "  }\n").concat(
+                        "]"
+                );
                 java.nio.file.Files.write(file.toPath(), defaultJson.getBytes("UTF-8"));
 
                 System.out.println("[Food Addition] created default foodaddition_potion_effects.json at: " + file.getAbsolutePath());
@@ -82,13 +69,21 @@ public class PotionEffectHandler {
             Type listType = new TypeToken<ArrayList<FoodEffectEntry>>() {}.getType();
             try (FileReader reader = new FileReader(file)) {
                 List<FoodEffectEntry> entries = gson.fromJson(reader, listType);
-                if (entries != null) configEntries.addAll(entries);
+                if (entries != null) {
+                    for (FoodEffectEntry entry : entries) {
+                        String key = entry.item.concat("@").concat(String.valueOf(entry.meta));
+                        List<PotionEffect> potionEffects = new ArrayList<>();
+                        for (EffectEntry effect : entry.effects) {
+                            potionEffects.add(new PotionEffect(effect.id, effect.duration * 20, effect.amplifier));
+                        }
+                        effectMap.put(key, potionEffects);
+                    }
+                }
             }
         } catch (Exception e) {
             System.err.println("[Food Addition] failed to load potion effects config: " + e.getMessage());
         }
     }
-
 
     public void addPotionEffectToPlayer(EntityPlayer player, List<PotionEffect> effects) {
         effects.forEach(player::addPotionEffect);
