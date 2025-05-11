af383ad3e7e0fbbd53a9ef971818d534ef35e810
Thenemex <tnmx777@gmail.com>
2025-05-12 01:23:09 +0200

    Split methods in Effect Handler - Implemented toString() for the Entry types for the json parsing

--- src/main/java/foodaddition/api/handlers/effects/types/EffectEntry.java
+++ src/main/java/foodaddition/api/handlers/effects/types/EffectEntry.java
@@ -4,4 +4,12 @@ public class EffectEntry {
     public int id;
     public int duration;
     public int amplifier;
+
+    @Override
+    public String toString() {
+        return getClass().getSimpleName()
+                .concat(" : ").concat(String.valueOf(id))
+                .concat(" - ").concat(String.valueOf(duration))
+                .concat("s - lvl.").concat(String.valueOf(amplifier));
+    }
 }
--- src/main/java/foodaddition/api/handlers/effects/types/FoodEffectEntry.java
+++ src/main/java/foodaddition/api/handlers/effects/types/FoodEffectEntry.java
@@ -6,4 +6,12 @@ public class FoodEffectEntry {
     public String item;
     public int meta;
     public List<EffectEntry> effects;
+
+    @Override
+    public String toString() {
+        return getClass().getSimpleName()
+                .concat(" : ").concat(item)
+                .concat(" - ").concat(String.valueOf(meta))
+                .concat(" - ").concat(effects.toString());
+    }
 }
