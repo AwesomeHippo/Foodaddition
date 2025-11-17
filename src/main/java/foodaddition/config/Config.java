5aff3973930037607eee08bbef332d81c5c44e5e
Thenemex <tnmx777@gmail.com>
2025-11-17 10:11:38 +0100

    Edited config again with new category

--- src/main/java/foodaddition/config/Config.java
+++ src/main/java/foodaddition/config/Config.java
@@ -19,7 +19,7 @@ public class Config {
     }
 
     private static void loadConfig() {
-        String itemsCat = "Items", modOptions = "Mod-Options", integs = "Integrations";
+        String itemsCat = "Items", modOptions = "Mod-Options", grassDrops = "Grass-Drops", integs = "Integrations";
         config.addCustomCategoryComment(itemsCat, "Here you can disable/enable back food items. Change value to false to disable the food linked to the animal.");
         sheepFoodEnabled = config.get(itemsCat, "Sheep", true).getBoolean(true);
         squidFoodEnabled = config.get(itemsCat, "Squid", true).getBoolean(true);
@@ -28,8 +28,9 @@ public class Config {
         config.addCustomCategoryComment(modOptions, "Here you can turn on/off mechanics added my the mod");
         customMobDropsEnabled = config.get(modOptions, "Custom Drops", true, "Turn off/on new raw meat items dropped by the right mobs when killed").getBoolean(true);
         potionEffectsEnabled = config.get(modOptions, "Potion Effects", false, "Turn on/off to generate a new json file, that will allow you to quickly add potion effects of your choice upon eating food items of your choice").getBoolean(false);
-        wheatDropFromGrass = config.get(modOptions, "Wheat Drop From Grass", true).getBoolean(true);
-        wheatDropWeight = config.get(modOptions, "Wheat Drop chance", 2, "Wheat drop chance from grass. 1 means 1/80 or 1.25% ; 10 means 1/8 or 12.5%", 1, 10).getInt(2);
+        config.addCustomCategoryComment(grassDrops, "Here you can tweak new drops added when breaking grass");
+        wheatDropFromGrass = config.get(grassDrops, "Wheat Drop From Grass", true).getBoolean(true);
+        wheatDropWeight = config.get(grassDrops, "Wheat Drop chance", 2, "Wheat drop chance from grass. 1 means 1/80 or 1.25% ; 10 means 1/8 or 12.5%", 1, 10).getInt(2);
         config.addCustomCategoryComment(integs, "Here you can tweak which integrations you want running or not.");
         thaumcraftIntegrationEnabled = config.get(integs, "Thaumcraft", true, "Set aspects to new items added").getBoolean(true);
     }
