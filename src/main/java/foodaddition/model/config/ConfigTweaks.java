9e7ab01dc4aa2b82651b26476128a513352d14cf
Thenemex <tnmx777@gmail.com>
2025-11-17 10:03:01 +0100

    Adding new code from making Wheat directly drop from breaking grass, with a 1/40 <=> 2.5% chance

---
+++ src/main/java/foodaddition/model/config/ConfigTweaks.java
@@ -0,0 +1,16 @@
+package foodaddition.model.config;
+
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.common.MinecraftForge;
+
+public class ConfigTweaks {
+
+    public static void init() {
+        addWheatToGrassDrops();
+    }
+
+    protected static void addWheatToGrassDrops() {
+        MinecraftForge.addGrassSeed(new ItemStack(Items.wheat),2);
+    }
+}
