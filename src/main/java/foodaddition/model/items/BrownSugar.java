d442b12b774ae80436373da8362c0b3012ba922a
Thenemex <tnmx777@gmail.com>
2026-07-03 13:30:48 +0200

    Brown sugar eating animation is now 3 times much quicker + set potion effet to Speed I (0:02)

--- src/main/java/foodaddition/model/items/BrownSugar.java
+++ src/main/java/foodaddition/model/items/BrownSugar.java
@@ -1,10 +1,19 @@
 package foodaddition.model.items;
 
 import foodaddition.api.items.ItemFoodPlus;
+import net.minecraft.item.ItemStack;
+import net.minecraft.potion.Potion;
 
 public class BrownSugar extends ItemFoodPlus {
     // RGB : 175, 110, 75 // 0.5 Food + 0.2 Saturation
     public BrownSugar() {
         super(1, 0.2F, "brown_sugar");
+        // Speed, 2 seconds, lvl.1, 100%
+        setPotionEffect(Potion.moveSpeed.id, 2, 0, 1.0F);
+    }
+
+    @Override
+    public int getMaxItemUseDuration(ItemStack stack) {
+        return 10;
     }
 }
