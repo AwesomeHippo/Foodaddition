984920c52c18cba25fb0504068f021d34fff3b14
Thenemex <tnmx777@gmail.com>
2026-07-03 04:29:49 +0200

    Added recipes for brown sugar integration

--- src/main/java/foodaddition/model/config/ConfigItems.java
+++ src/main/java/foodaddition/model/config/ConfigItems.java
@@ -4,6 +4,10 @@ import cpw.mods.fml.common.registry.GameRegistry;
 import foodaddition.api.config.ConfigMeatDropItems;
 import foodaddition.api.items.ItemFoodPlus;
 import foodaddition.model.items.BrownSugar;
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.crafting.CraftingManager;
+import net.minecraft.item.crafting.ShapedRecipes;
 
 public class ConfigItems {
 
@@ -15,7 +19,26 @@ public class ConfigItems {
     }
 
     protected static void initBrownSugar() {
+        // Registering item
         brownSugar = new BrownSugar();
         GameRegistry.registerItem(brownSugar, brownSugar.getUnlocalizedName());
+
+        // Removing recipe for white sugar
+        {
+        ShapedRecipes sugarRecipe = null;
+        for (Object o : CraftingManager.getInstance().getRecipeList())
+            if (o instanceof ShapedRecipes) {
+                ShapedRecipes r = (ShapedRecipes) o;
+                if (r.getRecipeOutput() != null & r.getRecipeOutput().getItem() != null
+                        && r.getRecipeOutput().getItem().equals(Items.sugar) && r.getRecipeOutput().stackSize == 1 && r.recipeItems[0].getItem().equals(Items.reeds))
+                    sugarRecipe = r;
+            }
+        if (sugarRecipe == null) throw new RuntimeException("Recipe for sugar not found, report to author");
+        else CraftingManager.getInstance().getRecipeList().remove(sugarRecipe);}
+
+        // Adding recipe for reed -> brown sugar
+        GameRegistry.addShapelessRecipe(new ItemStack(brownSugar), new ItemStack(Items.reeds));
+        // Adding recipe for brown sugar -> sugar
+        GameRegistry.addShapelessRecipe(new ItemStack(Items.sugar), new ItemStack(brownSugar));
     }
 }
